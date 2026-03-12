1c9d8baee2664a7484c7c40b86bd3a91d59f0561
Jack Schumacher <jackrschumacher@gmail.com>
2026-03-12 17:31:27 -0400

    fix: Added ALTER Statements, Fixed formatting

--- Scripts/Create-Tables.sql
+++ Scripts/Create-Tables.sql
@@ -9,31 +9,6 @@ InsID char(3) NOT NULL,
 InsName varchar(15) NOT NULL,
 PRIMARY KEY (InsID));
 
-/* Create the inspecting table
- * InspectorID (FK from inspector table)
- * BuildingID (FK from building table)
- * DateLast
- * DateNext
- */
-CREATE TABLE inspecting(
-InsID char(3) NOT NULL,
-BuildingID char(3) NOT NULL,
-DateNext DATE NOT NULL,
-DateLast DATE NOT NULL,
-FOREIGN KEY (InsID) REFERENCES inspector(InsID),
-FOREIGN KEY (BuildingID) REFERENCES building(BuildingID));
-
-/* Create the building table
- * BuildingID (PK)
- * BNoOfFloors
- * BManagerID (FK from manager table)
- */
-CREATE TABLE building(
-BuildingID char(3) NOT NULL,
-BNoOfFloors INT NOT NULL,
-BManagerID char(4) NOT NULL,
-FOREIGN KEY (BManagerID) REFERENCES manager(ManagerID));
-
 /* Create a manager table
  * ManagerID (PK)
  * MFName 
@@ -51,4 +26,44 @@ MBDate DATE NOT NULL,
 MSalary Numeric(9,2) NOT NULL,
 MBonus Numeric(9,2) NULL,
 MResBuildingID char(3) NOT NULL,
-FOREIGN KEY (MResBuildingID) REFERENCES building(BuildingID));
+PRIMARY KEY (ManagerID));
+
+
+
+/* Create the building table
+ * BuildingID (PK)
+ * BNoOfFloors
+ * BManagerID (FK from manager table)
+ */
+CREATE TABLE building(
+BuildingID char(3) NOT NULL,
+BNoOfFloors INT NOT NULL,
+BManagerID char(4) NOT NULL,
+PRIMARY KEY (BuildingID));
+
+
+/* Create the inspecting table
+ * InspectorID (FK from inspector table)
+ * BuildingID (FK from building table)
+ * DateLast
+ * DateNext
+ */
+CREATE TABLE inspecting(
+InsID char(3) NOT NULL,
+BuildingID char(3) NOT NULL,
+DateNext DATE NOT NULL,
+DateLast DATE NOT NULL,
+PRIMARY KEY (InsID)
+);
+
+-- Alter tables to allow for insertion of FKs when there are circular dependencies
+
+ALTER TABLE inspecting
+ADD FOREIGN KEY (InsID) REFERENCES inspector(InsID);
+ADD FOREIGN KEY (BuildingID) REFERENCES building(BuildingID);
+
+ALTER TABLE manager
+ADD FOREIGN KEY (MResBuildingID) REFERENCES building(BuildingID);
+
+ALTER TABLE building
+ADD FOREIGN KEY (BManagerID) REFERENCES manager(ManagerID);

e9b5b4fb3f17d30e10aeb1cd89a35308fe936ce3
Jack Schumacher <jackrschumacher@gmail.com>
2026-04-02 10:56:01 -0500

    Loading data

--- Scripts/Load-Data.sql
+++ Scripts/Load-Data.sql
@@ -34,9 +34,27 @@ ROWTERMINATOR = '\n',
 FIRSTROW = 2,
 TABLOCK);
 
--- Insert data from .csv into the managerphone table
+-- Insert data from .csv into the building table
 BULK INSERT building
 FROM "C:\Users\jackr\Documents\UAH\UAH-IS-340-Project-1\Data\building.csv"
+WITH (
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '\n',
+FIRSTROW = 2,
+TABLOCK);
+
+-- Insert data from .csv into the apartment table
+BULK INSERT apartment
+FROM "C:\Users\jackr\Documents\UAH\UAH-IS-340-Project-1\Data\apartment.csv"
+WITH (
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '\n',
+FIRSTROW = 2,
+TABLOCK);
+
+-- Insert data from .csv into the cleaning table
+BULK INSERT cleaning
+FROM "C:\Users\jackr\Documents\UAH\UAH-IS-340-Project-1\Data\cleaning.csv"
 WITH (
 FIELDTERMINATOR = ',',
 ROWTERMINATOR = '\n',

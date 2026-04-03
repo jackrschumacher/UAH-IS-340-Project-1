2d7ad69b9da5c1ef2641870ed988fd6802f3b757
Jack Schumacher <jackrschumacher@gmail.com>
2026-04-03 02:50:48 -0500

    feat: Creating Queries

---
+++ Scripts/Queries.sql
@@ -0,0 +1,52 @@
+USE HAFH;
+
+-- Display the total amount HAFH spends on manager salaries (as TotalSalary) and the total amount that HAFH spends on manager bonuses (as TotalBonus)
+SELECT SUM(MSalary) AS TotalSalary,
+SUM(MBonus) AS TotalBonus
+FROM manager;
+
+-- Display the ManagerID, MFName, MLName, MSalary and MBonus for all managers with a salary greater than $50,000 and a bonus of greater than $1,000
+SELECT ManagerID, MFName, MLName, MSalary, MBonus
+FROM manager
+WHERE MSalary > 50000 AND MBonus > 1000;
+
+-- Display the BuildingID, BNoOfFloors, managers MFName and MLName for all buildings. Shows all buildingIDs, will return nulls in right table if found
+SELECT 
+	building.BuildingID,
+	building.BNoOfFloors,
+	manager.MFName,
+	manager.MLName
+FROM
+	building
+LEFT OUTER JOIN 
+	manager 
+	ON
+	building.BManagerID = manager.ManagerID;
+
+-- Display BuildingID and AptNo leased by CorprateClient WindyCT
+SELECT 
+	apartment.BuildingID,
+	apartment.AptNo
+FROM
+	apartment
+LEFT OUTER JOIN
+	corpclient 
+	ON
+	apartment.CCID = corpclient.CCID
+WHERE
+	corpclient.CCNAME = 'WindyCT';
+
+-- Display InsID and InsName for all inspectors that have inspections scheduled after 1-April-2020. Only show unique inspections
+SELECT DISTINCT
+	inspector.InsID,
+	inspector.InsName
+FROM
+	inspector
+FULL OUTER JOIN
+	inspecting
+	ON
+	inspector.InsID = inspecting.InsID
+WHERE
+	DateNext > '2020-04-01';
+
+-- Display the ManagerID, MFName, MLName, MSalary, and MBonusfor the manager with the lowest total compensation (defined as salary plus bonus)
